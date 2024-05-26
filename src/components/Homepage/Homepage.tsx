0afe6d18cf0e9b91e707e5670a4dcaf25e88fb56
Michael DeMarco <michaelfromyeg@gmail.com>
2024-05-26 09:17:34 -0700

    feat: copywriting update

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -12,18 +12,17 @@ import Projects from "../Resume/Projects/Projects";
 import Extracurriculars from "../Resume/Activities/Activities";
 
 import Redirects from "../Redirects/Redirects";
-import Footer from "../Footer/Footer"
+import Footer from "../Footer/Footer";
 
 import Logo from "../../../content/assets/mjd-logo-black.svg";
-import { UNIVERSITY_YEAR_AS_STRING } from "../../data/Constants";
 
 const Homepage: React.FC = (): ReactElement => {
     const [index, setIndex] = useState(0);
 
     const strings = [
         "Michael DeMarco",
-        "an aspiring software developer",
-        `a UBC ${UNIVERSITY_YEAR_AS_STRING}`,
+        "a software engineer at Notion",
+        "passionate about teaching, mentorship, and research",
         "excited to meet you!",
     ];
 
@@ -42,28 +41,29 @@ const Homepage: React.FC = (): ReactElement => {
             <Links location={Locations.HOMEPAGE} />
             <SEO title="Home" />
             <div className="logo-text-wrapper">
-            <LogoComponent
-                alt="Michael's logo"
-                className="background-logo shadow"
-            />
-            <h1 className="title-text">
-                Hi, {`I'm`}
-                <Typist
-                    className="typist"
-                    onTypingDone={onComplete}
-                    stdTypingDelay={25}
-                    key={index}
-                >
-                    {`>`}{strings[index]}
-                    <Typist.Backspace
-                        count={strings[index].length}
-                        delay={3000}
-                    />
-                </Typist>
-            </h1>
+                <LogoComponent
+                    alt="Michael's logo"
+                    className="background-logo shadow"
+                />
+                <h1 className="title-text">
+                    Hi, {`I'm`}
+                    <Typist
+                        className="typist"
+                        onTypingDone={onComplete}
+                        stdTypingDelay={25}
+                        key={index}
+                    >
+                        {`>`}
+                        {strings[index]}
+                        <Typist.Backspace
+                            count={strings[index].length}
+                            delay={3000}
+                        />
+                    </Typist>
+                </h1>
             </div>
-            {/* <Arrow /> */}
-            <br /><br />
+            <br />
+            <br />
             <Redirects />
             <div className="resume">
                 <Education />
